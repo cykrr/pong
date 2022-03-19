172939bb85072035bf8ca29121b81710323a15d4
Your Name <you@example.com>
2022-03-19 16:04:51 -0300

    add sdl win render

--- main.c
+++ main.c
@@ -1,6 +1,31 @@
+#include <SDL2/SDL.h>
 #include <stdio.h>
 #include <stdlib.h>
 int main()
 {
+	if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
+		fprintf(stderr, "SDL_Init Error: %s\n", SDL_GetError());
+		return EXIT_FAILURE;
+	}
+
+	SDL_Window* win = SDL_CreateWindow("Hello World!", 100, 100, 640, 480, SDL_WINDOW_SHOWN);
+	if (win == NULL) {
+		fprintf(stderr, "SDL_CreateWindow Error: %s\n", SDL_GetError());
+		return EXIT_FAILURE;
+	}
+
+	SDL_Renderer* ren = SDL_CreateRenderer(win, -1,
+			SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+
+	if (ren == NULL) {
+		fprintf(stderr, "SDL_CreateRenderer Error: %s\n", SDL_GetError());
+		SDL_DestroyWindow(win);
+		SDL_Quit();
+		return EXIT_FAILURE;
+	}
+	SDL_DestroyRenderer(ren);
+	SDL_DestroyWindow(win);
+	SDL_Quit();
+
 	return EXIT_SUCCESS;
 }
