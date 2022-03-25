4f1e1913a5d7b89e96afb63a88f494fed947d149
Your Name <you@example.com>
2022-03-25 00:11:55 -0300

    indent

--- main.c
+++ main.c
@@ -66,22 +66,37 @@ void updateTime() {
 int main()
 {
 	if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
-		fprintf(stderr, "SDL_Init Error: %s\n", SDL_GetError());
+		fprintf(stderr, 
+                        "SDL_Init Error: %s\n",
+                        SDL_GetError());
+
+		SDL_Quit();
 		return EXIT_FAILURE;
 	}
 
-	SDL_Window* win = SDL_CreateWindow("Hello World!", 100, 100, 500, 500,
+	SDL_Window* win = SDL_CreateWindow(
+                "Hello World!", 100, 100, 500, 500,
                 SDL_WINDOW_SHOWN| SDL_WINDOW_OPENGL );
+
 	if (win == NULL) {
-		fprintf(stderr, "SDL_CreateWindow Error: %s\n", SDL_GetError());
+		fprintf(stderr, 
+                        "SDL_CreateWindow Error: %s\n",
+                        SDL_GetError());
+
+		SDL_DestroyWindow(win);
+		SDL_Quit();
 		return EXIT_FAILURE;
 	}
 
 	SDL_Renderer* ren = SDL_CreateRenderer(win, -1,
-			SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+			SDL_RENDERER_ACCELERATED | 
+                        SDL_RENDERER_PRESENTVSYNC);
 
 	if (ren == NULL) {
-		fprintf(stderr, "SDL_CreateRenderer Error: %s\n", SDL_GetError());
+		fprintf(stderr,
+                        "SDL_CreateRenderer Error: %s\n",
+                        SDL_GetError());
+
 		SDL_DestroyWindow(win);
 		SDL_Quit();
 		return EXIT_FAILURE;
